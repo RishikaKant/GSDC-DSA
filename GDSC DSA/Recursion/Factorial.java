12cd7b0bafccf2f5c43ba5129037f7ac846140f4
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2021-11-12 16:05:57 +0530

    Add files via upload

--- GDSC DSA/Recursion/Factorial.java
+++ GDSC DSA/Recursion/Factorial.java
@@ -1,3 +1,12 @@
 public class Factorial {
-    
+    static long factorial(int N) {
+        long ans = 1L;
+        if (N == 1 || N == 0) {
+            return 1;
+        }
+        ans = N * factorial(N - 1);
+        return ans;
+        // code here
+    }
+
 }
