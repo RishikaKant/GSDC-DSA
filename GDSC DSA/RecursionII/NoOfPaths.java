e4aa789b6f3fbd0d1e9a42ac1c232582d0bf0203
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2021-12-04 18:18:10 +0530

    Add files via upload

---
+++ GDSC DSA/RecursionII/NoOfPaths.java
@@ -0,0 +1,11 @@
+public class NoOfPaths {
+    class Solution {
+        long numberOfPaths(int m, int n) {
+            if (m == 1 || n == 1) {
+                return 1;
+            }
+
+            return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
+        }
+    }
+}
